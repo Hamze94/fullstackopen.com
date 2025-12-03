65e3d9ec0fb8dfdcc16b8fefcdc8eed5674a2edf
hamze Mohamed <hamzemohamed9412@gmail.com>
2025-12-04 00:13:01 +0100

    2.10: The Phonebook Step 5

--- part2/The Phonebook/src/App.jsx
+++ part2/The Phonebook/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
@@ -6,52 +9,20 @@ const App = () => {
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    const isDuplicate = persons.some(person => person.name === newName)
-    if (isDuplicate) {
-      alert(`${newName} is already added to phonebook`)
-      return
-    }
-    const personObject = { name: newName, number: newNumber }
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
-  }
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
-  const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value)
-  }
   const filteredPersons = persons.filter(person =>
     person.name.toLowerCase().includes(searchTerm.toLowerCase())
   )
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>filter shown with:<input value={searchTerm} onChange={handleSearchChange} /> </div>
+      <Filter searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       <h2>Add a new</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
-        <div>
-          <button onClick={handleSubmit} type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm persons={persons} setPersons={setPersons} />
       <h2>Numbers</h2>
       list of persons
-      {filteredPersons.map((person, index) =>
-        <p key={index}>{person.name} {person.number}</p>
-      )}
+      <Persons persons={filteredPersons} />
     </div>
   )
 }
---
+++ part2/The Phonebook/src/components/PersonForm.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+const PersonForm = ({ persons, setPersons }) => {
+    const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const isDuplicate = persons.some(person => person.name === newName)
+        if (isDuplicate) {
+            alert(`${newName} is already added to phonebook`)
+            return
+        }
+        const personObject = { name: newName, number: newNumber }
+        setPersons(persons.concat(personObject))
+        setNewName('')
+        setNewNumber('')
+    }
+    const handleNameChange = (event) => {
+        setNewName(event.target.value)
+    }
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value)
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    name: <input value={newName} onChange={handleNameChange} />
+                </div>
+                <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+                <div>
+                    <button onClick={handleSubmit} type="submit">add</button>
+                </div>
+            </form>
+
+        </>
+    )
+}
+
+export default PersonForm
