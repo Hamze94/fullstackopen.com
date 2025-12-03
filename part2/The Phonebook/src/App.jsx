fe6ee8ba2b0ad7ab6fa597de0657e537c5512288
hamze Mohamed <hamzemohamed9412@gmail.com>
2025-12-03 23:29:27 +0100

    2.9*: The Phonebook Step 4

--- part2/The Phonebook/src/App.jsx
+++ part2/The Phonebook/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
-
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-123456' },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
   const handleSubmit = (event) => {
     event.preventDefault()
     const isDuplicate = persons.some(person => person.name === newName)
@@ -24,9 +27,17 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value)
+  }
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  )
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with:<input value={searchTerm} onChange={handleSearchChange} /> </div>
+      <h2>Add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -38,7 +49,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       list of persons
-      {persons.map((person, index) =>
+      {filteredPersons.map((person, index) =>
         <p key={index}>{person.name} {person.phone}</p>
       )}
     </div>
