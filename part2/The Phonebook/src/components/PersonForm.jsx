f82e2540b30927381170f540065cc8336e8a293f
hamze Mohamed <hamzemohamed9412@gmail.com>
2025-12-06 01:54:39 +0100

    2.15*: The Phonebook step 10

--- part2/The Phonebook/src/components/PersonForm.jsx
+++ part2/The Phonebook/src/components/PersonForm.jsx
@@ -7,11 +7,19 @@ const PersonForm = ({ persons, setPersons }) => {
     const [newNumber, setNewNumber] = useState('')
     const handleSubmit = (event) => {
         event.preventDefault()
-        const isDuplicate = persons.some(person => person.name === newName)
-        if (isDuplicate) {
-            alert(`${newName} is already added to phonebook`)
+        const existingPerson = persons.find(person => person.name === newName)
+        if (existingPerson) {
+            if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+                const updatedPerson = { ...existingPerson, number: newNumber }
+                contactService.update(existingPerson.id, updatedPerson).then(returnedPerson => {
+                    setPersons(persons.map(person => person.id !== existingPerson.id ? person : returnedPerson))
+                    setNewName('')
+                    setNewNumber('')
+                })
+            }
             return
         }
+
         const personObject = { name: newName, number: newNumber }
         contactService.create(personObject).then(returnedPerson => {
             setPersons(persons.concat(returnedPerson))
