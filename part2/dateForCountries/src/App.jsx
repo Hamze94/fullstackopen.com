122de3df9f3300559d8cf23a3de8ccac7dad781b
hamze Mohamed <hamzemohamed9412@gmail.com>
2025-12-09 00:38:09 +0100

    2.18* Data for countries, step 1

--- part2/dateForCountries/src/App.jsx
+++ part2/dateForCountries/src/App.jsx
@@ -1,19 +1,61 @@
-import { use, useState } from 'react'
+import { useEffect, useState } from 'react'
 import React from 'react'
 import axios from 'axios'
 function App() {
   const [searchTerm, setSearchTerm] = useState('')
+  const [filteredCountries, setFilteredCountries] = useState([])
+  const [allCountries, setAllCountries] = useState([])
+
+  useEffect(() => {
+    axios.get('https://studies.cs.helsinki.fi/restcountries/api/all').then((response) => {
+      console.log(response.data)
+      setAllCountries(response.data)
+    })
+  }, [])
+  useEffect(() => {
+    if (searchTerm === '') {
+      setFilteredCountries([])
+    } else {
+      const filter = allCountries.filter(country =>
+        country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      setFilteredCountries(filter)
+    }
+
+  }, [searchTerm, allCountries]);
   const handleChange = (event) => {
     setSearchTerm(event.target.value)
-    use(axios.get('https://restcountries.com/v3.1/all')).then((response) => {
-      console.log(response.data)
-    })
+
   }
   return (
     <>
       <div>
         find countries
         <input onChange={handleChange} value={searchTerm} />
+        {
+          (filteredCountries.length > 10) ? (
+            <p>Too many matches, specify another filter</p>
+          ) : (filteredCountries.length === 1) ? (
+            <div>
+              <h1>{filteredCountries[0].name.common}</h1>
+              <p>capital {filteredCountries[0].capital}</p>
+              <p>area {filteredCountries[0].area}</p>
+              <h2>languages:</h2>
+              <ul>
+                {
+                  Object.values(filteredCountries[0].languages).map((language) => (
+                    <li key={language}>{language}</li>
+                  ))
+                }
+              </ul>
+              <img src={filteredCountries[0].flags.png} alt="flag" width="150" />
+            </div>
+          ) : (
+            filteredCountries.map((country) => (
+              <p key={country.name.common}>{country.name.common}</p>
+            ))
+          )
+        }
       </div>
     </>
   )
