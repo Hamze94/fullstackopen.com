d82daf9d4f0a47f62d1e12538820f9d7267ebbc1
hamze Mohamed <hamzemohamed9412@gmail.com>
2025-12-14 18:15:18 +0100

    updated the port in backend

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan')
 const cors = require('cors');
 const app = express();
-const port = 3001;
+const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cors());
 
@@ -84,6 +84,6 @@ app.post('/api/persons', (request, response) => {
     persons = persons.concat(newPerson);
     response.status(201).json(newPerson);
 });
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
