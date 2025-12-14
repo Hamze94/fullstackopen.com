04ee5e0fa199ccfe3de766c9d3ff46a927bd7a6c
hamze Mohamed <hamzemohamed9412@gmail.com>
2025-12-14 17:42:04 +0100

    3.9 Phonebook backend step 9

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const morgan = require('morgan')
+const cors = require('cors');
 const app = express();
 const port = 3001;
 app.use(express.json());
+app.use(cors());
+
 app.use(morgan(function (tokens, req, res) {
     return [
         tokens.method(req, res),
