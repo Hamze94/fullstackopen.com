ec67e348715a7efa654667161d88146ee59a456f
hamze Mohamed <hamzemohamed9412@gmail.com>
2025-12-30 18:03:19 +0100

    4.5*: Helper Functions and Unit Tests, step 3

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -4,8 +4,11 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
-
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((fav, blog) => (blog.likes > fav.likes ? blog : fav), blogs[0])
+}
 module.exports = {
     dummy,
     totalLikes,
-}   
+    favoriteBlog
+}
