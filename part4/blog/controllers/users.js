256a5d4552d6dc668adba633ef04d6c9e7ce8788
Hamze Mohamed <hamzemohamed9412@gmail.com>
2026-01-23 00:51:05 +0100

    4.15: Blog List Expansion, step 3

--- part4/blog/controllers/users.js
+++ part4/blog/controllers/users.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const usersRouter = require('express').Router()
 const User = require('../models/User')
 usersRouter.post('/', async (request, response) => {
+    console.log(request.body);
     const { username, name, password } = request.body
     const salt = await bcrypt.genSalt(10);
     const passwordHash = await bcrypt.hash(password, salt);
@@ -12,6 +13,10 @@ usersRouter.post('/', async (request, response) => {
     })
     const savedUser = await user.save()
     response.status(201).json(savedUser)
+});
+usersRouter.get('/', async (request, response) => {
+    const users = await User.find({}).populate('blogs', { title: 1, author: 1, url: 1 })
+    response.json(users.map(u => u.toJSON()))
 })
 
 module.exports = usersRouter
