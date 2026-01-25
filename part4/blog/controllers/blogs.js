b8f0db6e194441b15530cbedff6c6cb6027538d0
Hamze Mohamed <hamzemohamed9412@gmail.com>
2026-01-25 18:18:05 +0100

    4.17: Blog List Expansion, step 5

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,18 +1,34 @@
 
 const blogsRouter = require('express').Router()
 const Blog = require('../models/Blog')
+const User = require('../models/User')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
     response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
     try {
-        const blog = new Blog(request.body)
+        const user = await User.findOne({})
+        if (!user) {
+            return response.status(400).json({
+                error: 'No users found. Create a user first.'
+            })
+        }
+        const blog = new Blog({
+            ...request.body,
+            user: user._id
+        })
         const savedBlog = await blog.save()
-        response.status(201).json(savedBlog)
+        user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
+        const populatedBlog = await Blog.findById(savedBlog._id)
+            .populate('user', { username: 1, name: 1 })
+
+        response.status(201).json(populatedBlog)
     } catch (error) {
+        console.error(error)
         response.status(400).json({ error: 'Bad Request' })
     }
 })
