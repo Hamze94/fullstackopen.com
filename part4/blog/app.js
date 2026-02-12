e4a224100dc9a215203070fb2c6068fdeb2613b8
Hamze Mohamed <hamzemohamed9412@gmail.com>
2026-02-12 21:32:33 +0100

    4.18: Blog List Expansion, step 6

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -4,6 +4,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login');
 const app = express()
 
 logger.info('connecting to', config.MONGODB_URL)
@@ -20,6 +21,7 @@ mongoose
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter);
 
 
 module.exports = app
